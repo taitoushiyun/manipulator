3180eff4ae74ec1a4dac0aab6c73b6224969546e
cq <1270088158@qq.com>
2020-12-17 21:56:46 +0800

    -

--- PPO/importmagic.py
+++ PPO/importmagic.py
@@ -1,3 +1,5 @@
 import sys
 
 sys.path.append('/home/wangyw/cq/code/manipulator')
+sys.path.append('/home/wangyw/cq/code/manipulator/PPO')
+sys.path.append('/home/wangyw/cq/code/manipulator/vrep_con')
