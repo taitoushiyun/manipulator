9f16e326eeec9e0e4612fba8b6f53571965c47f5
cq <1270088158@qq.com>
2021-02-01 11:23:47 +0800

    --

--- TD3/td3_run.sh
+++ TD3/td3_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python td3_run.py --code_version td3_29 --cc_model False --plane_model False
-python td3_run.py --code_version td3_30 --cc_model True --plane_model True
-pyhton td3_run.py --code_version td3_31 --cc_model True  --plane_model False
+python td3_run.py --code-version td3_51 --actor-hidden [200, 200]
+python td3_run.py --code-version td3_52 --gamma 0.99
