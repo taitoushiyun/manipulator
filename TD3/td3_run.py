466e335de5d8a3ca3582bc2fb94d83bcf2565755
cq <1270088158@qq.com>
2021-02-08 16:34:24 +0800

    72,73,75,76try and retry+td3_74_rt super hard 简单版本

--- TD3/td3_run.py
+++ TD3/td3_run.py
@@ -150,7 +150,7 @@ def playGame(args_, train=True, episode_count=2000):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='TD3 for manipulator.')
-    parser.add_argument('--code-version', type=str, default='td3_77')
+    parser.add_argument('--code-version', type=str, default='td3_74_rt')
     parser.add_argument('--vis-port', type=int, default=6016)
     parser.add_argument('--seed', type=int, default=1)
     #  TD3 config
@@ -175,10 +175,10 @@ if __name__ == "__main__":
     parser.add_argument('--plane-model', type=bool, default=True)
     parser.add_argument('--cc-model', type=bool, default=True)
     parser.add_argument('--goal-set', type=str, choices=['easy', 'hard', 'super hard', 'random'],
-                        default='easy')
+                        default='super hard')
     parser.add_argument('--collision-cnt', type=int, default=15)
     parser.add_argument('--scene-file', type=str, default='simple_12_1_cc.ttt')
-    parser.add_argument('--headless-mode', type=bool, default=True)
+    parser.add_argument('--headless-mode', type=bool, default=False)
 
     parser.add_argument('--train', type=bool, default=True)
     parser.add_argument('--load-model', type=str, default=None)
