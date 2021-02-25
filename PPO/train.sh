0607f0437927413a6375ab85a9a7508cc28017ee
cq <1270088158@qq.com>
2021-02-25 19:16:25 +0800

    mani_53 -mani_57 test gamma=0

--- PPO/train.sh
+++ PPO/train.sh
@@ -13,6 +13,7 @@
 #python train.py --headless-mode --train --code-version mani_51 --gamma 0.8 --goal-set 'hard' --batch-size 64
 #python train.py --headless-mode --train --code-version mani_52 --gamma 0.8 --goal-set 'hard' --batch-size 128
 #python train.py --headless-mode --train --code-version mani_53 --gamma 0.99 --max-episode-steps 20
-python train.py --train --code-version mani_54 --gamma 0.9 --max-episode-steps 20
-python train.py --train --code-version mani_55 --gamma 0.8 --max-episode-steps 20
-python train.py --train --code-version mani_56 --gamma 0.6 --max-episode-steps 20
+#python train.py --train --code-version mani_54 --gamma 0.9 --max-episode-steps 20
+#python train.py --train --code-version mani_55 --gamma 0.8 --max-episode-steps 20
+python train.py --train --code-version mani_56 --gamma 0.6 --max-episode-steps 20 --headless-mode &
+python train.py --train --code-version mani_57 --gamma 0.0 --max-episode-steps 20 --headless-mode &
